bb0767fb57ede822e97842953fe15ee4cc6ffa05
brLestad <45737112+brLestad@users.noreply.github.com>
2018-12-09 19:21:21 +0300

    Add files via upload

---
+++ YAŞ.py
@@ -0,0 +1,6 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ahmet = int(input("YASINIZ:"))
+berkay = int(input("YASINIZ:"))
+mehmet = int(input("YASINIZ"))
+print("YASLARINIZIN TOPLAMI:",ahmet+berkay+mehmet)
