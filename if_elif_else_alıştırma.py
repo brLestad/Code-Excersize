8e2fca8f4e11ec8c9ca4174d7737ddd433af1601
brLestad <45737112+brLestad@users.noreply.github.com>
2018-12-15 17:57:35 +0300

    Create if_elif_else_alıştırma.py

---
+++ if_elif_else_alıştırma.py
@@ -0,0 +1,13 @@
+note = int(input("NOTUNUZU GİRİNİZ: "))
+if (note >= 90):
+    print("AA")
+elif (note >= 80):
+    print("BB")
+elif (note >= 70):
+    print("CC")
+elif (note >= 60):
+    print("DD")
+elif (note >= 50):
+    print("DF")
+else:
+    print("FF yani KALDINIZ!")
