eeb19ad57857e66558fc0b7130c0c25724db88fa
brLestad <45737112+brLestad@users.noreply.github.com>
2018-12-21 18:17:09 +0300

    Create break_islemi.py

---
+++ break_islemi.py
@@ -0,0 +1,10 @@
+while True:
+    isim = input("İSMİNİZ(Çıkmak için 'Q' ya basınız.): ")
+    soyisim = input("SOYİSMİNİZ(Çıkmak için 'Q' ya basınız): ")
+    if (isim == "Q"):
+        print("PROGRAMDAN ÇIKILIYOR...")
+        break
+    elif (soyisim == "Q"):
+        print("PROGRAMDAN ÇIKILIYOR...")
+    else:
+        print("PROGRAMA GİRİŞ YAPILDI.")
